888bee06e5f92792ece208ab438e20069092571e
JWJung-99 <39busy@naver.com>
2024-10-07 23:42:10 +0900

    🚧 Day09: 백트래킹 문제 풀이 중

--- Day9/0만들기.js
+++ Day9/0만들기.js
@@ -20,3 +20,9 @@ let input = fs.readFileSync("../input.txt").toString().trim().split("\n");
 
 // let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let t = Number(input[0]);
+
+for (let i = 1; i <= t; i++) {
+  let n = Number(input[i]);
+}
