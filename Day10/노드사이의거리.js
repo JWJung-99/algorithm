c6415fde2bece9098dc5d6b6e853b4a71c09b64a
JWJung-99 <39busy@naver.com>
2024-11-08 12:39:44 +0900

    🚧 Day10: DFS 문제 풀이 중

--- Day10/노드사이의거리.js
+++ Day10/노드사이의거리.js
@@ -22,5 +22,34 @@ let input = fs.readFileSync("../input.txt").toString().trim().split("\n");
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [n, m] = input[0].split(" ").map(Number);
+let graph = new Array(n + 1);
 
-console.log(n, m);
+for (let i = 1; i <= n; i++) {
+  graph[i] = [];
+}
+
+for (let i = 1; i < n; i++) {
+  const [from, to, dist] = input[i].split(" ").map(Number);
+
+  graph[from].push([to, dist]);
+  graph[to].push([from, dist]);
+}
+
+const dfs = (graph, start, visited, distance, current) => {
+  if (visited[start]) return;
+
+  distance[start] = current;
+  visited[start] = true;
+
+  for (let [node, dist] of graph[start]) {
+    dfs(graph, node, visited, distance, current + dist);
+  }
+};
+
+for (let i = 0; i < m; i++) {
+  const [from, to] = input[n + i].split(" ").map(Number);
+  let visited = new Array(n + 1).fill(false);
+  let distance = new Array(n + 1).fill(0);
+  dfs(graph, from, visited, distance, 0);
+  console.log(distance[to]);
+}
