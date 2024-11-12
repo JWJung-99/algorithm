1a1eaed117b9233e1d1d8637f0c08284a3b5def2
JWJung-99 <39busy@naver.com>
2024-11-13 00:04:23 +0900

    ✅ Day10: DFS 트리 문제 풀이 완료

--- Day10/트리.js
+++ Day10/트리.js
@@ -19,14 +19,56 @@ let input = fs.readFileSync("../input.txt").toString().trim().split("\n");
 // let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-function isCycle(graph, x, prev, visited) {
-  visited[x] = true;
+function hasCycle(graph, node, prev, visited) {
+  visited[node] = true;
 
-  for (let y of graph[x]) {
-    if (!visited[y]) {
-      if (isCycle(graph, y, x, visited)) return true;
-    } else if (y != prev) return true;
+  for (let neighbor of graph[node]) {
+    if (!visited[neighbor]) {
+      if (hasCycle(graph, neighbor, node, visited)) return true;
+    } else if (neighbor !== prev) {
+      return true;
+    }
   }
 
   return false;
+}
+
+let line = 0;
+let testCase = 1;
+
+while (1) {
+  let [n, m] = input[line].split(" ").map(Number);
+
+  if (n === 0 && m === 0) break;
+
+  let visited = new Array(n + 1).fill(false);
+  let graph = [];
+
+  for (let i = 1; i <= n; i++) {
+    graph[i] = [];
+  }
+
+  for (let i = 0; i < m; i++) {
+    let [x, y] = input[line + 1 + i].split(" ").map(Number);
+    graph[x].push(y);
+    graph[y].push(x);
+  }
+
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    if (!visited[i] && !hasCycle(graph, i, 0, visited)) {
+      count++;
+    }
+  }
+
+  if (count > 1) {
+    console.log(`Case ${testCase}: A forest of ${count} trees.`);
+  } else if (count === 1) {
+    console.log(`Case ${testCase}: There is one tree.`);
+  } else {
+    console.log(`Case ${testCase}: No trees.`);
+  }
+
+  line += m + 1;
+  testCase++;
 }
