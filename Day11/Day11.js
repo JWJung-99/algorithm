e661ebae01ad1f0c5c26fcdf11622ec09e510d35
JWJung-99 <39busy@naver.com>
2024-11-23 18:58:56 +0900

    ✨ Day11: DFS 복습

---
+++ Day11/Day11.js
@@ -0,0 +1,14 @@
+/*
+[문제 설명]
+
+[입력]
+
+[출력]
+
+*/
+
+import fs from "fs";
+let input = fs.readFileSync("../input.txt").toString().trim().split("\n");
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
