afc4502818d40aa189a2f446b3e1a550a342cbfe
JWJung-99 <39busy@naver.com>
2024-12-11 00:56:21 +0900

    Done: 구명보트 다른 풀이

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,17 +1,35 @@
 function solution(people, limit) {
-    var answer = 0;
-    people.sort((a, b) => a - b);
-    
-    while (people.length > 0) {
-        if (people[0] + people[people.length - 1] <= limit) {
-            people.pop();
-            people.shift();
-        } else {
-            people.pop();
-        }
-        
-        answer++;
+  var answer = 0;
+  people.sort((a, b) => a - b);
+
+  while (people.length > 0) {
+    if (people[0] + people[people.length - 1] <= limit) {
+      people.pop();
+      people.shift();
+    } else {
+      people.pop();
     }
-    
-    return answer;
+
+    answer++;
+  }
+
+  return answer;
+}
+
+function solution(people, limit) {
+  var answer = 0;
+  people.sort((a, b) => a - b);
+  let left = 0;
+  let right = people.length - 1;
+
+  while (left <= right) {
+    if (people[left] + people[right] <= limit) {
+      left++;
+    }
+
+    right--;
+    answer++;
+  }
+
+  return answer;
 }
