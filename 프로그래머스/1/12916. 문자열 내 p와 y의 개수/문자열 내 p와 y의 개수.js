98f002cbb61f130af367cc4155a27646cf523021
진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-01-21 09:34:44 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,13 @@
+function solution(s){
+    
+    let p = 0;
+    let y = 0;
+    
+    s.toLowerCase().split('').forEach((char) => {
+        if (char === 'p') p++;
+        else if (char === 'y') y++;
+    })
+    
+
+    return p === y;
+}
