a40688d010c174b6862064eafb9ba402d1349814
진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-03-21 20:02:14 +0900

    [Silver III] Title: 01타일, Time: 224 ms, Memory: 87736 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let N = Number(input[0]);
+
+let d = new Array(10000001).fill(0);
+
+d[1] = 1;
+d[2] = 2;
+
+for (let i = 3; i <= N; i++) {
+  d[i] = (d[i - 1] + d[i - 2]) % 15746;
+}
+
+console.log(d[N]);
