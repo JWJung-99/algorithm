66554e3a3f523ae61321e7ee35e2e18168cb909e
진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-04-17 18:36:13 +0900

    [level 2] Title: 타겟 넘버, Time: 501.71 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,19 @@
-selected = []
+answer = 0
+
+def dfs(numbers, target, currentValue, depth): 
+    global answer
+    if depth == len(numbers):
+        if currentValue == target:
+            answer += 1
+        return
+
+    for oper in ['+', '-']:
+        if oper == '+':
+            dfs(numbers, target, currentValue + numbers[depth] * 1, depth + 1)
+        else:
+            dfs(numbers, target, currentValue - numbers[depth] * 1, depth + 1)
 
 def solution(numbers, target):    
-    def dfs(currentValue, depth):  
-        if depth == len(numbers):
-            selected.append(currentValue)
-            return
-        
-        for oper in ['+', '-']:
-            if oper == '+':
-                dfs(currentValue + numbers[depth] * 1, depth + 1)
-            else:
-                dfs(currentValue - numbers[depth] * 1, depth + 1)
-                
-    dfs(0, 0)
-    answer = selected.count(target)
+    dfs(numbers, target, 0, 0)
     
     return answer
