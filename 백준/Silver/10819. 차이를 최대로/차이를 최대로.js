8e89eadffcdea3b7f94b3fa6687e6085f94e9da5
진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-05-14 22:14:01 +0900

    [Silver II] Title: 차이를 최대로, Time: 168 ms, Memory: 12004 KB -BaekjoonHub

---
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
@@ -0,0 +1,37 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0]);
+const num = input[1].split(" ").map(Number);
+const visited = Array(N).fill(false);
+const selected = [];
+const answer = [];
+
+const calculateAnswer = (arr, num) => {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    sum += Math.abs(num[arr[i]] - num[arr[i + 1]]);
+  }
+
+  return sum;
+};
+
+const DFS = (depth) => {
+  if (depth === N) {
+    answer.push(calculateAnswer(selected, num));
+  }
+
+  for (let i = 0; i < N; i++) {
+    if (visited[i]) continue;
+
+    visited[i] = true;
+    selected.push(i);
+    DFS(depth + 1);
+    selected.pop();
+    visited[i] = false;
+  }
+};
+
+DFS(0);
+console.log(Math.max(...answer));
