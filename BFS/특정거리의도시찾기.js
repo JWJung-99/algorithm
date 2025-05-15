6333f9443f65293b3b5bd9a0707281cf0453a3e6
JWJung-99 <39busy@naver.com>
2025-05-16 00:02:11 +0900

    Cont: BFS 문제 풀이 중

---
+++ BFS/특정거리의도시찾기.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+let input = fs.readFileSync("../input.txt").toString().trim().split("\n");
+
+const [N, M, K, X] = input[0].split(" ").map(Number);
+const graph = Array.from(Array(N + 1), () => []);
+let distance = Array(N + 1).fill(-1);
+
+for (let i = 1; i <= M; i++) {
+  let [from, to] = input[i].split(" ").map(Number);
+
+  graph[from].push(to);
+}
+
+let queue = [];
+// 출발 도시, 현재 이동 거리
+distance[X] = 0;
+queue.push(X);
+
+while (queue.length > 0) {
+  let curX = queue.shift();
+
+  for (let neighbor of graph[curX]) {
+    if (distance[neighbor] !== -1) continue;
+
+    distance[neighbor] = distance[curX] + 1;
+    queue.push(neighbor);
+  }
+}
+
+let canReach = false;
+
+for (let i = 1; i <= N; i++) {
+  if (distance[i] === K) {
+    console.log(i);
+    canReach = true;
+  }
+}
+
+if (!canReach) console.log(-1);
---
+++ BFS/환승.js
@@ -0,0 +1,42 @@
+import fs from "fs";
+let input = fs.readFileSync("../input.txt").toString().trim().split("\n");
+
+const [N, K, M] = input[0].split(" ").map(Number);
+let graph = Array.from(Array(N + M + 1), () => []);
+let distance = Array(N + M + 1).fill(-1);
+
+for (let i = 1; i <= M; i++) {
+  let arr = input[i].split(" ").map(Number);
+
+  for (let x of arr) {
+    graph[x].push(N + i);
+    graph[N + i].push(x);
+  }
+}
+
+let queue = [];
+queue.push(1);
+distance[1] = 0;
+let found = false;
+
+while (queue.length > 0) {
+  let curPos = queue.shift();
+
+  if (curPos === N) {
+    found = true;
+    break;
+  }
+
+  for (let neighbor of graph[curPos]) {
+    if (distance[neighbor] === -1) {
+      distance[neighbor] = distance[curPos] + 1;
+      queue.push(neighbor);
+    }
+  }
+}
+
+if (found) {
+  console.log(distance[N] / 2 + 1);
+} else {
+  console.log(-1);
+}
