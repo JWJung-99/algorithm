0c036d748db0beebc5ac7a410e808d44256b4e2d
JWJung-99 <39busy@naver.com>
2025-08-23 22:50:51 +0900

    Lecture: 재귀함수와 완전탐색 문제풀이 완료

--- Lecture/Section8/Q15.js
+++ Lecture/Section8/Q15.js
@@ -2,12 +2,39 @@ import fs from 'fs';
 let input = fs.readFileSync('../../input.txt').toString().trim().split('\n');
 let testCase = Number(input[0]);
 
-function solution() {}
+/**
+ *
+ * @param {number} k
+ * @param {number[]} numbers
+ * @param {number} m
+ * @returns {number}
+ */
+function solution(k, numbers, m) {
+	let answer = 0;
+
+	function DFS(depth, s, sum) {
+		if (depth === k) {
+			if (sum % m === 0) answer++;
+			return;
+		} else {
+			for (let i = s; i < numbers.length; i++) {
+				DFS(depth + 1, i + 1, sum + numbers[i]);
+			}
+		}
+	}
+
+	DFS(0, 0, 0);
+
+	return answer;
+}
 
 let index = 1;
 while (testCase > 0) {
-	console.log(solution());
+	const [N, K] = input[index].split(' ').map(Number);
+	const numbers = input[index + 1].split(' ').map(Number);
+	const M = Number(input[index + 2]);
+	console.log(solution(K, numbers, M));
 
 	testCase--;
-	index += 2;
+	index += 3;
 }
