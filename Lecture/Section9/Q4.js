d6a050bf55e6c6ad64cb80c8236c98cf131d7c21
JWJung-99 <39busy@naver.com>
2025-08-24 21:07:24 +0900

    Lecture: 그래프와 탐색 문제풀이 중

---
+++ Lecture/Section9/Q4.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+let input = fs.readFileSync('../../input.txt').toString().trim().split('\n');
+let testCase = Number(input[0]);
+
+function solution() {}
+
+let index = 1;
+while (testCase > 0) {
+	console.log(solution());
+
+	testCase--;
+	index += 1;
+}
