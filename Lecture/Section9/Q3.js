fa8e9d4e913be0e8fd3c25ad20bbc20d28834873
JWJung-99 <39busy@naver.com>
2025-08-25 16:51:34 +0900

    Lecture: 그래프와 탐색 문제풀이 중

--- Lecture/Section9/Q3.js
+++ Lecture/Section9/Q3.js
@@ -2,12 +2,49 @@ import fs from 'fs';
 let input = fs.readFileSync('../../input.txt').toString().trim().split('\n');
 let testCase = Number(input[0]);
 
-function solution() {}
+/**
+ * @description 1번 정점에서 N번 정점으로 이동하는 가짓수 출력 (인접리스트 활용)
+ * @param {number} N 목적지
+ * @param {[number, number][]} graph 방향그래프
+ * @returns {number}
+ */
+function solution(N, arr) {
+	let answer = 0;
+	let graph = Array.from({ length: N + 1 }, () => []);
+	for (let [from, to] of arr) {
+		graph[from].push(to);
+	}
+
+	let visited = Array(N + 1).fill(false);
+	function DFS(v) {
+		if (v === N) answer++;
+		else {
+			for (let i = 0; i < graph[v].length; i++) {
+				let nextV = graph[v][i];
+				if (visited[nextV]) continue;
+
+				visited[nextV] = true;
+				DFS(nextV);
+				visited[nextV] = false;
+			}
+		}
+	}
+
+	visited[1] = true;
+	DFS(1);
+
+	return answer;
+}
 
 let index = 1;
 while (testCase > 0) {
-	console.log(solution());
+	const [N, M] = input[index].split(' ').map(Number);
+	const arr = [];
+	for (let i = index + 1; i <= index + M; i++) {
+		arr.push(input[i].split(' ').map(Number));
+	}
+	console.log(solution(N, arr));
 
 	testCase--;
-	index += 1;
+	index += M + 1;
 }
--- Lecture/Section9/Q4.js
+++ Lecture/Section9/Q4.js
@@ -2,12 +2,63 @@ import fs from 'fs';
 let input = fs.readFileSync('../../input.txt').toString().trim().split('\n');
 let testCase = Number(input[0]);
 
-function solution() {}
+/**
+ * (1, 1)에서 (7, 7)까지 갈 수 있는 경우의 수 출력
+ * @param {number[][]} graph 7*7 격자판의 정보
+ * @returns {number}
+ */
+function solution(graph) {
+	let answer = 0;
+
+	// [상, 하, 좌, 우]
+	let dx = [-1, 1, 0, 0];
+	let dy = [0, 0, -1, 1];
+
+	let path = [];
+
+	function DFS(x, y) {
+		if (x === 6 && y === 6) {
+			answer++;
+			console.log(path);
+		} else {
+			for (let i = 0; i < 4; i++) {
+				let nextX = x + dx[i];
+				let nextY = y + dy[i];
+
+				if (
+					nextX >= 0 &&
+					nextX < 7 &&
+					nextY >= 0 &&
+					nextY < 7 &&
+					graph[nextX][nextY] === 0
+				) {
+					graph[nextX][nextY] = 1;
+					path.push([nextX + 1, nextY + 1]);
+					DFS(nextX, nextY);
+					path.pop();
+					graph[nextX][nextY] = 0;
+				}
+			}
+		}
+	}
+
+	graph[0][0] = 1;
+	path.push([1, 1]);
+	DFS(0, 0);
+
+	return answer;
+}
 
 let index = 1;
 while (testCase > 0) {
-	console.log(solution());
+	const graph = [];
+
+	for (let i = index; i < index + 7; i++) {
+		graph.push(input[i].split(' ').map(Number));
+	}
+
+	console.log(solution(graph));
 
 	testCase--;
-	index += 1;
+	index += 7;
 }
