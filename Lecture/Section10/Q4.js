627236f5f0b88af5e9e379dcd28a9e5c1548e8bd
JWJung-99 <39busy@naver.com>
2025-08-29 01:14:40 +0900

    Lecture: 동적계획법 문제 풀이 완료

--- Lecture/Section10/Q4.js
+++ Lecture/Section10/Q4.js
@@ -2,12 +2,31 @@ import fs from 'fs';
 let input = fs.readFileSync('../../input.txt').toString().trim().split('\n');
 let testCase = Number(input[0]);
 
-function solution() {}
+/**
+ * @description [냅색] 여러 단위의 동전들이 주어져 있을 때 거슬러 줄 동전의 최소개수
+ * @param {number[]} coins
+ * @param {number} M
+ * @returns {number}
+ */
+function solution(coins, M) {
+	let dp = Array(M + 1).fill(500);
+	dp[0] = 0;
+
+	for (let coin of coins) {
+		for (let i = coin; i <= M; i++) {
+			dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+		}
+	}
+
+	return dp[M];
+}
 
 let index = 1;
 while (testCase > 0) {
-	console.log(solution());
+	const coins = input[index + 1].split(' ').map(Number);
+	const M = Number(input[index + 2]);
+	console.log(solution(coins, M));
 
 	testCase--;
-	index += 1;
+	index += 3;
 }
--- Lecture/Section10/Q5.js
+++ Lecture/Section10/Q5.js
@@ -2,12 +2,34 @@ import fs from 'fs';
 let input = fs.readFileSync('../../input.txt').toString().trim().split('\n');
 let testCase = Number(input[0]);
 
-function solution() {}
+/**
+ * @description [냅색] 제한시간 M안에 N개의 문제 중 얻을 수 있는 최대 점수
+ * @param {[number, number][]} questions
+ * @param {number} M
+ * @returns {number}
+ */
+function solution(questions, M) {
+	let dp = Array(M + 1).fill(0);
+
+	for (let [score, time] of questions) {
+		for (let i = M; i >= time; i--) {
+			dp[i] = Math.max(dp[i], dp[i - time] + score);
+		}
+		console.log(dp);
+	}
+
+	return dp[M];
+}
 
 let index = 1;
 while (testCase > 0) {
-	console.log(solution());
+	const [N, M] = input[index].split(' ').map(Number);
+	let questions = [];
+	for (let i = index + 1; i <= index + N; i++) {
+		questions.push(input[i].split(' ').map(Number));
+	}
+	console.log(solution(questions, M));
 
 	testCase--;
-	index += 1;
+	index += N + 1;
 }
