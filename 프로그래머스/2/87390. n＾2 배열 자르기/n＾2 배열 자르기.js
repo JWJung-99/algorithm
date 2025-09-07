6b8e480212dde3fb7cb7ed6da943a5d7700636b0
JWJung-99 <39busy@naver.com>
2025-09-07 22:19:25 +0900

    n^2 배열 자르기 코드 수정

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,11 +1,16 @@
 function solution(n, left, right) {
-    var answer = [];
-    
-    while (left <= right) {
-        // 2차원 배열의 값: 행, 열 중 큰 값 + 1
-        answer.push(Math.max(parseInt(left / n), left % n) + 1);
-        left++;
-    }
-    
-    return answer;
+	var answer = [];
+
+	/*
+    [1, 2, 3]
+    [2, 2, 3]
+    [3, 3, 3]
+    */
+	while (left <= right) {
+		// 2차원 배열의 값: 행, 열 중 큰 값 + 1
+		answer.push(Math.max(parseInt(left / n), left % n) + 1);
+		left++;
+	}
+
+	return answer;
 }
