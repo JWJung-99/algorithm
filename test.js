8241c385841074258a9f52eee8425752ce719f63
JWJung-99 <39busy@naver.com>
2025-09-12 18:59:26 +0900

    Programmers: 뒤에 있는 큰 수 찾기 첫 번째 시도 추가

--- test.js
+++ test.js
@@ -1,46 +1,20 @@
-import fs from 'fs';
-let input = fs.readFileSync('./input.txt').toString().trim().split('\n');
+const numbers = [9, 1, 5, 3, 6, 2];
 
-const dirs = input[0];
+function solution1(numbers) {
+	let answer = [];
 
-function solution1(dirs) {
-	let answer = 0;
-
-	let direction = {
-		U: [0, 1],
-		D: [0, -1],
-		R: [1, 0],
-		L: [-1, 0],
-	};
-
-	let visited = Array.from({ length: 11 }, () =>
-		Array.from({ length: 11 }, () => {
-			return {
-				U: false,
-				D: false,
-				R: false,
-				L: false,
-			};
-		})
-	);
-
-	let [curX, curY] = [5, 5];
-
-	for (let dir of dirs) {
-		let [nextX, nextY] = [curX + direction[dir][0], curY + direction[dir][1]];
-
-		if (nextX < 0 || nextX >= 11 || nextY < 0 || nextY >= 11) continue;
-
-		if (!visited[curX][curY][dir]) {
-			answer++;
-			visited[curX][curY][dir] = true;
+	for (let i = 0; i < numbers.length; i++) {
+		let bigNum = -1;
+		for (let j = i + 1; j < numbers.length; j++) {
+			if (numbers[j] > numbers[i]) {
+				bigNum = numbers[j];
+				break;
+			}
 		}
-
-		curX = nextX;
-		curY = nextY;
+		answer.push(bigNum);
 	}
 
 	return answer;
 }
 
-console.log(solution1(dirs));
+console.log(solution1(numbers));
--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,15 +1,33 @@
 function solution(numbers) {
-    let answer = Array(numbers.length).fill(-1);
-    let stack = [];
-    
-    for (let i = 0; i < numbers.length; i++) {
-        while (numbers[i] > numbers[stack.at(-1)]) {
-            let index = stack.pop();
-            answer[index] = numbers[i];
-        }
-        
-        stack.push(i);
-    }
-    
-    return answer;
+	let answer = Array(numbers.length).fill(-1);
+	let stack = [];
+
+	for (let i = 0; i < numbers.length; i++) {
+		while (numbers[i] > numbers[stack.at(-1)]) {
+			let index = stack.pop();
+			answer[index] = numbers[i];
+		}
+
+		stack.push(i);
+	}
+
+	return answer;
+}
+
+// 첫 번째 시도: 배열 순회 => O(n^2)로 효율성 문제
+function solution1(numbers) {
+	let answer = [];
+
+	for (let i = 0; i < numbers.length; i++) {
+		let bigNum = -1;
+		for (let j = i + 1; j < numbers.length; j++) {
+			if (numbers[j] > numbers[i]) {
+				bigNum = numbers[j];
+				break;
+			}
+		}
+		answer.push(bigNum);
+	}
+
+	return answer;
 }
