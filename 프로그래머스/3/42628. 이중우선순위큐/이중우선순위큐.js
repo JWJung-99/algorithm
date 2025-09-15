5e493c5615665441e8c0faab7f815d229a487802
JWJung-99 <39busy@naver.com>
2025-09-16 00:33:31 +0900

    Programmers: 이중우선순위큐 - 추후 Heap을 이용해 풀어보기

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,23 +1,35 @@
+/**
+ * @description 힙을 사용하지 않고 이중우선순위큐 구현
+ * @param {string[]} operations
+ * @returns {[number, number]}
+ */
 function solution(operations) {
-    let queue = [];    
-    
-    function operation(oper, num) {
-        switch(oper) {
-            case 'I':
-                queue.push(num);
-                break;
-            case 'D':
-                if (num === 1) queue.splice(queue.indexOf(Math.max(...queue)), 1);
-                else queue.splice(queue.indexOf(Math.min(...queue)), 1);
-        }
-    }
-    
-    for (let command of operations) {
-        let [oper, num] = command.split(' ');
-        operation(oper, +num);
-    }
-    
-    if (queue.length) {
-        return [Math.max(...queue), Math.min(...queue)];
-    } else return [0, 0];
+	let queue = [];
+
+	function operation(oper, num) {
+		switch (oper) {
+			case 'I':
+				queue.push(num);
+				break;
+			case 'D':
+				if (num === 1) queue.splice(queue.indexOf(Math.max(...queue)), 1);
+				else queue.splice(queue.indexOf(Math.min(...queue)), 1);
+		}
+	}
+
+	for (let command of operations) {
+		let [oper, num] = command.split(' ');
+		operation(oper, +num);
+	}
+
+	if (queue.length) {
+		return [Math.max(...queue), Math.min(...queue)];
+	} else return [0, 0];
 }
+
+/**
+ * @description Heap을 구현해서 풀이
+ * @param {string[]} operations
+ * @returns {[number, number]}
+ */
+function solution2(operations) {}
