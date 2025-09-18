578afaa118e5b7c00286ce38ea77a1f6e5ed7a63
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-09-18 14:11:18 +0900

    [level 2] Title: [3차] 압축, Time: 0.65 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -8,15 +8,18 @@ function solution(msg) {
 	}
 	let lastIndex = 27;
 
-	//
+	// 메시지 압축 - 첫 번째 글자부터 시작
 	let pointer = 0;
 	let chars = msg[pointer];
 	let result = 0;
 	while (pointer < msg.length) {
+        // 사전에 chars가 있다면 result 값 업데이트, chars에 다음 글자 추가
 		if (dictionary[chars]) {
 			result = dictionary[chars];
 			chars += msg[++pointer];
-		} else {
+		}
+        // 사전에 chars가 없다면 추가, answer에 result 값 추가, result 초기화, chars를 다음 위치로 업데이트
+        else {
 			dictionary[chars] = lastIndex++;
 			answer.push(result);
 			result = 0;
@@ -24,6 +27,7 @@ function solution(msg) {
 		}
 	}
 
+    // answer에 마지막 result 값 추가
 	answer.push(result);
 
 	return answer;
