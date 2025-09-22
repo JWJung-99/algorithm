3ba60fb48b7470eb81457e4cafc8eb7c0b69c069
JWJung-99 <39busy@naver.com>
2025-09-22 13:37:52 +0900

    Programmers: 등굣길 첫 번째 시도 추가

--- test.js
+++ test.js
@@ -1,33 +1,35 @@
-function solution(x, y, n) {
-	let answer = -1;
+function solution(m, n, puddles) {
+	let answer = 0;
 
+	let graph = Array.from({ length: m }, () => Array(n).fill(1));
+
+	// 물에 잠긴 지역 방문 방지
+	for (let [x, y] of puddles) {
+		graph[x - 1][y - 1] = 0;
+	}
+
+	let dx = [1, 0];
+	let dy = [0, 1];
 	let queue = [];
-	queue.push([x, 0]);
 
+	// 출발 노드 방문 처리
+	queue.push([0, 0, 0]);
+
+	// BFS
 	while (queue.length) {
-		let [cur, cnt] = queue.shift();
+		let [curX, curY, dist] = queue.shift();
 
-		if (cur === y) {
-			answer = cnt;
-			break;
-		}
+		if (curX === m - 1 && curY === n - 1) answer++;
 
-		for (let i = 0; i < 3; i++) {
-			let next;
+		for (let i = 0; i < 2; i++) {
+			let nextX = curX + dx[i];
+			let nextY = curY + dy[i];
 
-			if (i === 0) next = cur + n;
-			else if (i === 1) next = cur * 2;
-			else next = cur * 3;
+			if (nextX > m - 1 || nextY > n - 1 || graph[nextX][nextY] === 0) continue;
 
-			if (next <= y) {
-				queue.push([next, cnt + 1]);
-			}
+			queue.push([nextX, nextY, dist + 1]);
 		}
 	}
 
 	return answer;
 }
-
-console.log(solution(10, 40, 5));
-console.log(solution(10, 40, 30));
-console.log(solution(2, 5, 4));
--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,18 +1,56 @@
+// 오른쪽/아래쪽으로만 이동 -> 경로가 이전 상태에 의존하기 때문에 DP 풀이가 적절!
 function solution(m, n, puddles) {
-    let dp = Array.from({length: m}, () => Array(n).fill(0));
-    dp[0][0] = 1;
-    
-    for (let [x, y] of puddles) dp[x - 1][y - 1] = -1;
-    
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (i === 0 && j === 0) continue;
-            if (dp[i][j] === -1) continue;
-            
-            if (i > 0 && dp[i - 1][j] > 0) dp[i][j] += dp[i - 1][j] % 1000000007;
-            if (j > 0 && dp[i][j - 1] > 0) dp[i][j] += dp[i][j - 1] % 1000000007;
-        }
-    }
-    
-    return dp[m - 1][n - 1] % 1000000007;
+	let dp = Array.from({ length: m }, () => Array(n).fill(0));
+	dp[0][0] = 1;
+
+	for (let [x, y] of puddles) dp[x - 1][y - 1] = -1;
+
+	for (let i = 0; i < m; i++) {
+		for (let j = 0; j < n; j++) {
+			if (i === 0 && j === 0) continue;
+			if (dp[i][j] === -1) continue;
+
+			if (i > 0 && dp[i - 1][j] > 0) dp[i][j] += dp[i - 1][j] % 1000000007;
+			if (j > 0 && dp[i][j - 1] > 0) dp[i][j] += dp[i][j - 1] % 1000000007;
+		}
+	}
+
+	return dp[m - 1][n - 1] % 1000000007;
+}
+
+// BFS 풀이 -> 시간 초과
+function solution(m, n, puddles) {
+	let answer = 0;
+
+	let graph = Array.from({ length: m }, () => Array(n).fill(1));
+
+	// 물에 잠긴 지역 방문 방지
+	for (let [x, y] of puddles) {
+		graph[x - 1][y - 1] = 0;
+	}
+
+	let dx = [1, 0];
+	let dy = [0, 1];
+	let queue = [];
+
+	// 출발 노드 방문 처리
+	queue.push([0, 0, 0]);
+
+	// BFS
+	while (queue.length) {
+		let [curX, curY, dist] = queue.shift();
+
+		if (curX === m - 1 && curY === n - 1) answer++;
+
+		for (let i = 0; i < 2; i++) {
+			let nextX = curX + dx[i];
+			let nextY = curY + dy[i];
+
+			if (nextX > m - 1 || nextY > n - 1 || graph[nextX][nextY] === 0) continue;
+
+			queue.push([nextX, nextY, dist + 1]);
+		}
+	}
+
+	return answer;
 }
