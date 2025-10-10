bcb104d6397758331e995f854ef434c2fb7e6eeb
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-10-10 20:00:04 +0900

    [level 2] Title: 피로도, Time: 13.56 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -0,0 +1,38 @@
+function solution(k, dungeons) {
+    let n = dungeons.length;
+    let visited = Array(n).fill(false);
+    let selected = [];
+    let maxResult = 0;
+    
+    function DFS(depth) {
+        if (depth === n) {
+            let remaining = k;
+            let result = 0;
+            
+            for (let index of selected) {
+                let [minimum, usage] = dungeons[index];
+                
+                if (remaining < minimum) continue;
+                
+                remaining -= usage;
+                result++;
+            }
+            
+            if (result > maxResult) maxResult = result;
+        } else {
+            for (let i = 0; i < n; i++) {
+                if (visited[i]) continue;
+                
+                visited[i] = true;
+                selected.push(i);
+                DFS(depth + 1);
+                selected.pop();
+                visited[i] = false;
+            }
+        }
+    }
+    
+    DFS(0);
+    
+    return maxResult;
+}
