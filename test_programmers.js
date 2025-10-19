3c9162c6e60bd7c324624be9d4550e0a5fb08167
JWJung-99 <39busy@naver.com>
2025-10-19 22:53:02 +0900

    Test: 프로그래머스 테스트 파일

--- test_programmers.js
+++ test_programmers.js
@@ -1,3 +1,36 @@
-function solution() {}
+// idea: 각 바위 사이의 거리를 기준으로 이분탐색
+function solution(distance, rocks, n) {
+	rocks.sort((a, b) => a - b);
 
-console.log(solution());
+	let start = 1;
+	let end = distance;
+
+	let answer = 0;
+
+	while (start <= end) {
+		let mid = parseInt((start + end) / 2);
+
+		let removed = 0;
+		let index = 0;
+
+		// 각 바위 사이의 거리가 mid보다 작거나 같다면 제거
+		for (let rock of rocks) {
+			if (rock - index < mid) removed++;
+			else index = rock;
+		}
+
+		if (distance - index < mid) removed++;
+
+		// n개보다 많이 제거했다면 기준 거리를 줄여야 함
+		if (removed > n) {
+			end = mid - 1;
+		} else {
+			start = mid + 1;
+			answer = mid;
+		}
+	}
+
+	return answer;
+}
+
+console.log(solution(25, [2, 14, 11, 21, 17], 2));
