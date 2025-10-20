bae97a0a238e48d9e59260ae20e9b919c9fb0ba6
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-10-21 01:47:58 +0900

    [Gold IV] Title: 알파벳, Time: 6872 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.js
+++ 백준/Gold/1987. 알파벳/알파벳.js
@@ -1,32 +1,33 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [R, C] = input[0].split(" ").map(Number);
-const board = [];
+let [R, C] = input[0].split(' ').map(Number);
+let board = [];
 for (let i = 1; i <= R; i++) {
-  board.push(input[i].split(""));
-}
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
-let visited = new Set();
-let max = 0;
-
-function dfs(depth, x, y) {
-  max = Math.max(max, depth);
-
-  for (let i = 0; i < 4; i++) {
-    let nextX = x + dx[i];
-    let nextY = y + dy[i];
-
-    if (nextX < 0 || nextY < 0 || nextX >= R || nextY >= C) continue;
-    if (visited.has(board[nextX][nextY])) continue;
-
-    visited.add(board[nextX][nextY]);
-    dfs(depth + 1, nextX, nextY);
-    visited.delete(board[nextX][nextY]);
-  }
+	board.push(input[i].split(''));
 }
 
-visited.add(board[0][0]);
-dfs(1, 0, 0);
-console.log(max);
+let dx = [1, 0, -1, 0];
+let dy = [0, 1, 0, -1];
+
+let answer = 0;
+let records = new Set(board[0][0]);
+
+function DFS(n, curX, curY) {
+	answer = Math.max(answer, n);
+
+	for (let i = 0; i < 4; i++) {
+		let nextX = curX + dx[i];
+		let nextY = curY + dy[i];
+
+		if (nextX < 0 || nextX >= R || nextY < 0 || nextY >= C) continue;
+		if (!records.has(board[nextX][nextY])) {
+			records.add(board[nextX][nextY]);
+			DFS(n + 1, nextX, nextY);
+			records.delete(board[nextX][nextY]);
+		}
+	}
+}
+
+DFS(1, 0, 0);
+console.log(answer);
