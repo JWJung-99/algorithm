cb2880da953cd8144dc4c10ecb712c982b7599ce
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-10-20 12:41:27 +0900

    [Silver III] Title: 모든 순열, Time: 184 ms, Memory: 18092 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.js
@@ -0,0 +1,27 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let N = Number(input[0]);
+
+let visited = Array(N + 1).fill(false);
+let selected = [];
+let answer = [];
+
+function DFS(n) {
+	if (n === N) {
+		answer.push(selected.join(' '));
+	} else {
+		for (let i = 1; i <= N; i++) {
+			if (visited[i]) continue;
+
+			visited[i] = true;
+			selected.push(i);
+			DFS(n + 1);
+			selected.pop();
+			visited[i] = false;
+		}
+	}
+}
+
+DFS(0);
+console.log(answer.join('\n'));
