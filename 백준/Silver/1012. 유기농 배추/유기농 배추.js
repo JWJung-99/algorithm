77054209dc971052bf48c26875d339d176fb711d
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-10-21 14:51:00 +0900

    [Silver II] Title: 유기농 배추, Time: 152 ms, Memory: 12516 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,49 +1,46 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let T = Number(input[0]);
-let line = 1;
+let testCase = Number(input[0]);
+let index = 1;
 
-function dfs(graph, m, n, x, y) {
-  if (x <= -1 || x >= m || y <= -1 || y >= n) return false;
+while (testCase > 0) {
+	let [M, N, K] = input[index].split(' ').map(Number);
+	let field = Array.from({ length: N }, () => Array(M).fill(0));
 
-  if (graph[x][y] === 1) {
-    // 해당 노드 방문 처리
-    graph[x][y] = -1;
+	for (let i = index + 1; i <= index + K; i++) {
+		let [c, r] = input[i].split(' ').map(Number);
+		field[r][c] = 1;
+	}
 
-    // 상, 하, 좌, 우 dfs 호출
-    dfs(graph, m, n, x - 1, y);
-    dfs(graph, m, n, x + 1, y);
-    dfs(graph, m, n, x, y - 1);
-    dfs(graph, m, n, x, y + 1);
+	let dx = [-1, 0, 1, 0];
+	let dy = [0, 1, 0, -1];
 
-    return true;
-  }
+	function DFS(x, y) {
+		for (let i = 0; i < 4; i++) {
+			let nextX = x + dx[i];
+			let nextY = y + dy[i];
 
-  return false;
-}
+			if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) continue;
+			if (field[nextX][nextY] === 1) {
+				field[nextX][nextY] = 0;
+				DFS(nextX, nextY);
+			}
+		}
+	}
 
-while (T > 0) {
-  const [m, n, k] = input[line].split(" ").map(Number);
+	let answer = 0;
+	for (let i = 0; i < N; i++) {
+		for (let j = 0; j < M; j++) {
+			if (field[i][j] === 1) {
+				answer++;
+				DFS(i, j);
+			}
+		}
+	}
 
-  let graph = [];
-  for (let i = 0; i < m; i++) {
-    graph[i] = new Array(n);
-  }
+	console.log(answer);
 
-  for (let i = 1; i <= k; i++) {
-    let [x, y] = input[line + i].split(" ").map(Number);
-    graph[x][y] = 1;
-  }
-
-  let answer = 0;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (dfs(graph, m, n, i, j)) answer++;
-    }
-  }
-  console.log(answer);
-
-  line += k + 1;
-  T--;
+	index += K + 1;
+	testCase--;
 }
