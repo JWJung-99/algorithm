95229249e11c487065a747d857a9a968f801424b
JWJung-99 <39busy@naver.com>
2025-10-23 22:21:08 +0900

    Test: 백준 테스트코드 수정

--- test_baekjoon.js
+++ test_baekjoon.js
@@ -3,29 +3,3 @@ let input = fs.readFileSync('./input.txt').toString().trim().split('\n');
 
 // let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-
-let [N, M] = input[0].split(' ').map(Number);
-
-let visited = Array(N + 1).fill(false);
-let selected = [];
-let answer = [];
-
-function DFS(n) {
-	if (n === M) {
-		answer.push(selected.join(' '));
-	} else {
-		for (let i = 1; i <= N; i++) {
-			if (visited[i]) continue;
-
-			visited[i] = true;
-			selected.push(i);
-			DFS(n + 1);
-			selected.pop();
-			visited[i] = false;
-		}
-	}
-}
-
-DFS(0);
-
-console.log(answer.join('\n'));
