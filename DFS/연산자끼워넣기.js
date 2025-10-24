ef3c4c7f190943fdfb702ba8805b1073094f44de
JWJung-99 <39busy@naver.com>
2025-10-24 18:44:42 +0900

    Baekjoon: 연산자끼워넣기 풀이 수정

--- DFS/연산자끼워넣기.js
+++ DFS/연산자끼워넣기.js
@@ -1,43 +1,44 @@
-import fs from "fs";
-let input = fs.readFileSync("../input.txt").toString().trim().split("\n");
+import fs from 'fs';
+let input = fs.readFileSync('../input.txt').toString().trim().split('\n');
 
-const N = Number(input[0]);
-const num = input[1].split(" ").map(Number);
-let [add, sub, mul, div] = input[2].split(" ").map(Number);
+let N = Number(input[0]);
+let numbers = input[1].split(' ').map(Number);
+let [add, sub, mul, div] = input[2].split(' ').map(Number);
+let min = 1e9;
+let max = -1e9;
 
-let minValue = 1e9;
-let maxValue = -1e9;
+function DFS(n, value) {
+	if (n === N) {
+		min = Math.min(min, value);
+		max = Math.max(max, value);
+		return;
+	} else {
+		if (add > 0) {
+			add--;
+			DFS(n + 1, value + numbers[n]);
+			add++;
+		}
 
-const DFS = (index, value) => {
-  if (index === N) {
-    minValue = Math.min(minValue, value);
-    maxValue = Math.max(maxValue, value);
-    return;
-  }
+		if (sub > 0) {
+			sub--;
+			DFS(n + 1, value - numbers[n]);
+			sub++;
+		}
 
-  if (add > 0) {
-    add--;
-    DFS(index + 1, value + num[index]);
-    add++;
-  }
-  if (sub > 0) {
-    sub--;
-    DFS(index + 1, value - num[index]);
-    sub++;
-  }
-  if (mul > 0) {
-    mul--;
-    DFS(index + 1, value * num[index]);
-    mul++;
-  }
-  if (div > 0) {
-    div--;
-    DFS(index + 1, ~~(value / num[index]));
-    div++;
-  }
-};
+		if (mul > 0) {
+			mul--;
+			DFS(n + 1, value * numbers[n]);
+			mul++;
+		}
 
-DFS(1, num[0]);
+		if (div > 0) {
+			div--;
+			DFS(n + 1, ~~(value / numbers[n]));
+			div++;
+		}
+	}
+}
 
-console.log(maxValue);
-console.log(minValue);
+DFS(1, numbers[0]);
+console.log(max);
+console.log(min);
