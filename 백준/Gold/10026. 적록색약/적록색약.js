856dbf414a8cb401bb601497e45708e8fc46a286
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-10-24 16:01:18 +0900

    [Gold V] Title: 적록색약, Time: 160 ms, Memory: 11212 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.js
+++ 백준/Gold/10026. 적록색약/적록색약.js
@@ -2,62 +2,63 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 let N = Number(input[0]);
-let artwork = Array.from(Array(N), () => []);
-let visited = Array.from(Array(N), () => Array(N).fill(false));
-
+let painting = [];
 for (let i = 1; i <= N; i++) {
-  artwork[i - 1] = input[i].split("");
+	painting.push(input[i].split(''));
 }
 
-let dx = [1, 0, -1, 0];
+let section = 0;
+let sectionForWeakness = 0;
+
+let visited = Array.from({ length: N }, () => Array(N).fill(false));
+
+let dx = [-1, 0, 1, 0];
 let dy = [0, 1, 0, -1];
 
-let rgSection = 0;
-let section = 0;
+function DFS(x, y) {
+	visited[x][y] = true;
 
-function dfs(x, y) {
-  if (!visited[x][y]) {
-    visited[x][y] = true;
+	for (let i = 0; i < 4; i++) {
+		let nextX = x + dx[i];
+		let nextY = y + dy[i];
 
-    for (let i = 0; i < 4; i++) {
-      const nextX = x + dx[i];
-      const nextY = y + dy[i];
+		if (
+			nextX < 0 ||
+			nextX >= N ||
+			nextY < 0 ||
+			nextY >= N ||
+			visited[nextX][nextY]
+		)
+			continue;
 
-      if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) continue;
-
-      if (artwork[x][y] === artwork[nextX][nextY]) dfs(nextX, nextY);
-    }
-  }
-}
-
-// 적록색약이 아닌 사람
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    if (!visited[i][j]) {
-      section++;
-      dfs(i, j);
-    }
-  }
-}
-
-// 적록색약인 사람
-visited = Array.from(Array(N), () => Array(N).fill(false));
-
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    if (artwork[i][j] === "R") {
-      artwork[i][j] = "G";
-    }
-  }
+		if (painting[nextX][nextY] === painting[x][y]) DFS(nextX, nextY);
+	}
 }
 
 for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    if (!visited[i][j]) {
-      rgSection++;
-      dfs(i, j);
-    }
-  }
+	for (let j = 0; j < N; j++) {
+		if (!visited[i][j]) {
+			section++;
+			DFS(i, j);
+		}
+	}
 }
 
-console.log(section, rgSection);
+visited = Array.from({ length: N }, () => Array(N).fill(false));
+
+for (let i = 0; i < N; i++) {
+	for (let j = 0; j < N; j++) {
+		if (painting[i][j] === 'G') painting[i][j] = 'R';
+	}
+}
+
+for (let i = 0; i < N; i++) {
+	for (let j = 0; j < N; j++) {
+		if (!visited[i][j]) {
+			sectionForWeakness++;
+			DFS(i, j);
+		}
+	}
+}
+
+console.log(section, sectionForWeakness);
