a86176eabe3ede0e6656a784ea9ec60a57522fc3
JWJung-99 <39busy@naver.com>
2025-10-27 18:48:00 +0900

    Baekjoon: 특정 거리의 도시 찾기 풀이 수정

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.js
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.js
@@ -1,38 +1,37 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const [N, M, K, X] = input[0].split(" ").map(Number);
-const graph = Array.from(Array(N + 1), () => []);
-let distance = Array(N + 1).fill(-1);
 
+let [N, M, K, X] = input[0].split(' ').map(Number);
+
+let graph = Array.from({ length: N + 1 }, () => []);
 for (let i = 1; i <= M; i++) {
-  let [from, to] = input[i].split(" ").map(Number);
+	let [A, B] = input[i].split(' ').map(Number);
 
-  graph[from].push(to);
+	graph[A].push(B);
 }
 
-let queue = [];
-// 출발 도시, 현재 이동 거리
+let distance = Array(N + 1).fill(-1);
+let queue = [X];
 distance[X] = 0;
-queue.push(X);
 
-while (queue.length > 0) {
-  let curX = queue.shift();
+while (queue.length) {
+	let current = queue.shift();
 
-  for (let neighbor of graph[curX]) {
-    if (distance[neighbor] !== -1) continue;
-
-    distance[neighbor] = distance[curX] + 1;
-    queue.push(neighbor);
-  }
+	for (let neighbor of graph[current]) {
+		if (distance[neighbor] === -1) {
+			distance[neighbor] = distance[current] + 1;
+			queue.push(neighbor);
+		}
+	}
 }
 
 let canReach = false;
 
 for (let i = 1; i <= N; i++) {
-  if (distance[i] === K) {
-    console.log(i);
-    canReach = true;
-  }
+	if (distance[i] === K) {
+		console.log(i);
+		canReach = true;
+	}
 }
 
 if (!canReach) console.log(-1);
