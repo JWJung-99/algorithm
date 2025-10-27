140ca79a03c0743e42b568e3f1a123acae02c6fb
JWJung-99 <39busy@naver.com>
2025-10-27 18:13:55 +0900

    Baekjoon: 경쟁적 전염 풀이 수정

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.js
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.js
@@ -1,50 +1,41 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [N, K] = input[0].split(" ").map(Number);
+let [N, K] = input[0].split(' ').map(Number);
 let graph = [];
-let data = [];
-
+let virus = [];
 for (let i = 0; i < N; i++) {
-  graph.push(input[i + 1].split(" ").map(Number));
-
-  for (let j = 0; j < N; j++) {
-    if (graph[i][j] != 0) {
-      // [바이러스 종류, 현재 시간, x 위치, y 위치]
-      data.push([graph[i][j], 0, i, j]);
-    }
-  }
+	graph.push(input[i + 1].split(' ').map(Number));
+	for (let j = 0; j < N; j++) {
+		if (graph[i][j] !== 0) virus.push([graph[i][j], 0, i, j]);
+	}
 }
+let [S, X, Y] = input[N + 1].split(' ').map(Number);
 
-const [S, X, Y] = input[N + 1].split(" ").map(Number);
-
-// 바이러스 번호가 작은 순서부터 queue에 삽입
-data.sort((a, b) => a[0] - b[0]);
+let dx = [-1, 0, 1, 0];
+let dy = [0, 1, 0, -1];
 
 let queue = [];
-for (let item of data) {
-  queue.push(item);
-}
 
-const dx = [1, 0, -1, 0];
-const dy = [0, 1, 0, -1];
+virus.sort((a, b) => a[0] - b[0]);
+for (let x of virus) queue.push(x);
 
-while (queue.length > 0) {
-  let [virus, s, x, y] = queue.shift();
+while (queue.length) {
+	let [curK, time, curX, curY] = queue.shift();
 
-  if (s === S) break;
+	if (time === S) break;
 
-  for (let i = 0; i < 4; i++) {
-    let nextX = x + dx[i];
-    let nextY = y + dy[i];
+	for (let i = 0; i < 4; i++) {
+		let nextX = curX + dx[i];
+		let nextY = curY + dy[i];
 
-    if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) continue;
+		if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) continue;
 
-    if (graph[nextX][nextY] === 0) {
-      graph[nextX][nextY] = virus;
-      queue.push([virus, s + 1, nextX, nextY]);
-    }
-  }
+		if (graph[nextX][nextY] === 0) {
+			graph[nextX][nextY] = curK;
+			queue.push([curK, time + 1, nextX, nextY]);
+		}
+	}
 }
 
 console.log(graph[X - 1][Y - 1]);
