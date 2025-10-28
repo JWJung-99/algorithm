e8ad1b13c5b96868e4c7d8dae86fa6402875a715
JWJung-99 <39busy@naver.com>
2025-10-28 13:35:31 +0900

    Baekjoon: A -> B 풀이 추가

--- 백준/Silver/16953. A → B/A → B.js
+++ 백준/Silver/16953. A → B/A → B.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
+// 1. 그리디 알고리즘
 let [A, B] = input[0].split(' ').map(Number);
 let answer = 1;
 let hasFound = false;
@@ -23,3 +24,30 @@ while (B >= A) {
 if (!hasFound) answer = -1;
 
 console.log(answer);
+
+// 2. BFS
+let [A2, B2] = input[0].split(' ').map(Number);
+
+let visited = new Set([A2]);
+let queue = [[A2, 1]];
+let hasFound2 = false;
+
+while (queue.length) {
+	let [curNum, curDist] = queue.shift();
+
+	if (curNum === B2) {
+		console.log(curDist);
+		hasFound2 = true;
+		break;
+	}
+
+	for (let nextNum of [curNum * 2, curNum * 10 + 1]) {
+		if (visited.has(nextNum)) continue;
+		if (nextNum > B2) continue;
+
+		visited.add(nextNum);
+		queue.push([nextNum, curDist + 1]);
+	}
+}
+
+if (!hasFound2) console.log(-1);
