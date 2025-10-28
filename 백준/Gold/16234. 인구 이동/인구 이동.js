13363fbd7badaddf8d8915c5bdf826395ed19c70
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-10-28 14:37:58 +0900

    [Gold IV] Title: 인구 이동, Time: 972 ms, Memory: 20320 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.js
+++ 백준/Gold/16234. 인구 이동/인구 이동.js
@@ -1,73 +1,63 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [N, L, R] = input[0].split(" ").map(Number);
-let graph = [];
-
-for (let i = 0; i < N; i++) {
-  graph.push(input[i + 1].split(" ").map(Number));
+let [N, L, R] = input[0].split(' ').map(Number);
+let maps = [];
+for (let i = 1; i <= N; i++) {
+	maps.push(input[i].split(' ').map(Number));
 }
 
-const dx = [1, 0, -1, 0];
-const dy = [0, 1, 0, -1];
+let dx = [-1, 0, 1, 0];
+let dy = [0, 1, 0, -1];
 
-const BFS = (x, y, visited) => {
-  let merged = [[x, y]];
-  visited[x][y] = true;
-  let sum = graph[x][y];
+function BFS(x, y, index, union) {
+	let united = [[x, y]];
+	let queue = [[x, y]];
+	union[x][y] = index;
 
-  let queue = [];
-  queue.push([x, y]);
+	let sum = maps[x][y];
+	let cnt = 1;
 
-  while (queue.length > 0) {
-    let [curX, curY] = queue.shift();
+	while (queue.length) {
+		let [curX, curY] = queue.shift();
 
-    for (let i = 0; i < 4; i++) {
-      let nextX = curX + dx[i];
-      let nextY = curY + dy[i];
+		for (let i = 0; i < 4; i++) {
+			let nextX = curX + dx[i];
+			let nextY = curY + dy[i];
 
-      if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) continue;
-      
-      if (!visited[nextX][nextY]) {
-        const diff = Math.abs(graph[nextX][nextY] - graph[curX][curY]);
+			if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) continue;
 
-        if (diff >= L && diff <= R) {
-          queue.push([nextX, nextY]);
-          merged.push([nextX, nextY]);
-          visited[nextX][nextY] = true;
-          sum += graph[nextX][nextY];
-        }
-      }
-    }
-  }
+			if (union[nextX][nextY] === -1) {
+				let diff = Math.abs(maps[nextX][nextY] - maps[curX][curY]);
+				if (diff >= L && diff <= R) {
+					united.push([nextX, nextY]);
+					sum += maps[nextX][nextY];
+					cnt += 1;
+					queue.push([nextX, nextY]);
+					union[nextX][nextY] = index;
+				}
+			}
+		}
+	}
 
-  const avg = parseInt(sum / merged.length);
+	for (let [i, j] of united) maps[i][j] = parseInt(sum / cnt);
+}
 
-  for (let country of merged) {
-    let [x, y] = country;
-    graph[x][y] = avg;
-  }
-};
-
-let day = 0;
-
+let answer = 0;
 while (true) {
-  let visited = Array.from(Array(N), () => Array(N).fill(false));
-  let count = 0;
+	let union = Array.from({ length: N }, () => Array(N).fill(-1));
+	let index = 0;
+	for (let i = 0; i < N; i++) {
+		for (let j = 0; j < N; j++) {
+			if (union[i][j] === -1) {
+				BFS(i, j, index, union);
+				index++;
+			}
+		}
+	}
 
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-      if (!visited[i][j]) {
-        BFS(i, j, visited);
-        count++;
-      }
-    }
-  }
-
-  // 모든 국가에 대해 한 번씩 BFS 시행 => 연합할 국가가 없음
-  if (count === N * N) break;
-
-  day += 1;
+	if (index === N * N) break;
+	answer += 1;
 }
 
-console.log(day);
+console.log(answer);
