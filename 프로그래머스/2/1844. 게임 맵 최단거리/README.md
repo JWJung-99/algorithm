19c0a9377672f8f19106215e20ceea95278a147f
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-10-29 12:54:29 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 30.68 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 37.6 MB, 시간: 31.16 ms
+메모리: 37.5 MB, 시간: 30.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 06일 01:06:06
+2025년 10월 29일 12:54:25
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,32 +1,36 @@
 function solution(maps) {
-    let n = maps.length;
-    let m = maps[0].length;
-    
-    let dx = [-1, 0, 1, 0];
-    let dy = [0, 1, 0, -1];
-    
-    let visited = Array.from({ length: n }, () => Array(m).fill(0));
-    
-    let queue = [];
-    
-    visited[0][0] = 1;
-    queue.push([0, 0, 1]);
-    
-    while (queue.length) {
-        let [curX, curY, cnt] = queue.shift();
-        
-        for (let i = 0; i < 4; i++) {
-            let nextX = curX + dx[i];
-            let nextY = curY + dy[i];
-            
-            if (nextX < 0 || nextX >= n || nextY < 0 || nextY > m) continue;
-            
-            if (visited[nextX][nextY] === 0 && maps[nextX][nextY] === 1) {
-                visited[nextX][nextY] = visited[curX][curY] + 1;
-                queue.push([nextX, nextY, cnt + 1]);
-            }
-        }
-    }
-    
-    return visited[n - 1][m - 1] ? visited[n - 1][m - 1] : -1;
+	let N = maps.length;
+	let M = maps[0].length;
+
+	let distance = Array.from({ length: N }, () => Array(M).fill(-1));
+	distance[0][0] = 1;
+
+	let dx = [-1, 0, 1, 0];
+	let dy = [0, -1, 0, 1];
+
+	let queue = [[0, 0]];
+	while (queue.length) {
+		let [curX, curY] = queue.shift();
+
+		for (let i = 0; i < 4; i++) {
+			let nextX = curX + dx[i];
+			let nextY = curY + dy[i];
+
+			if (
+				nextX < 0 ||
+				nextX >= N ||
+				nextY < 0 ||
+				nextY >= M ||
+				maps[nextX][nextY] === 0
+			)
+				continue;
+
+			if (distance[nextX][nextY] === -1) {
+				distance[nextX][nextY] = distance[curX][curY] + 1;
+				queue.push([nextX, nextY]);
+			}
+		}
+	}
+
+	return distance[N - 1][M - 1];
 }
