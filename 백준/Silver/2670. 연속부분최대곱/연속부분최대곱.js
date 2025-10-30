eecb5bb7bb2e253e9592bd0eddefe19b6288cc1d
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-10-31 00:33:22 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 156 ms, Memory: 12116 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.js
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.js
@@ -1,12 +1,16 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const N = Number(input[0]);
-const dp = Array(N).fill(0);
-dp[0] = Number(input[1]);
+let N = Number(input[0]);
+let numbers = [];
+for (let i = 1; i <= N; i++) {
+	numbers.push(Number(input[i]));
+}
 
+let dp = Array(N).fill(0);
+dp[0] = numbers[0];
 for (let i = 1; i < N; i++) {
-  dp[i] = Math.max(Number(input[i + 1]), dp[i - 1] * Number(input[i + 1]));
+	dp[i] = Math.max(dp[i - 1] * numbers[i], numbers[i]);
 }
 
 console.log(Math.max(...dp).toFixed(3));
