f947fd7895f009de6c3963be20a74085dece7f02
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-10-31 00:21:40 +0900

    [Silver I] Title: 정수 삼각형, Time: 172 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
@@ -1,33 +1,19 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const n = Number(input[0]);
+let N = Number(input[0]);
 
-let pyramid = [];
-
-for (let i = 1; i <= n; i++) {
-  pyramid.push(input[i].split(" ").map(Number));
+let numbers = [];
+for (let i = 1; i <= N; i++) {
+	numbers.push(input[i].split(' ').map(Number));
 }
 
-let dp = [];
-for (let i = 1; i <= n; i++) {
-  dp.push(Array(i).fill(0));
+for (let i = 1; i < N; i++) {
+	for (let j = 0; j <= i; j++) {
+		if (j === 0) numbers[i][j] += numbers[i - 1][j];
+		else if (j === i) numbers[i][j] += numbers[i - 1][j - 1];
+		else numbers[i][j] += Math.max(numbers[i - 1][j - 1], numbers[i - 1][j]);
+	}
 }
 
-dp[0][0] = pyramid[0][0];
-
-if (n > 1) {
-  dp[1][0] = dp[0][0] + pyramid[1][0];
-  dp[1][1] = dp[0][0] + pyramid[1][1];
-
-  for (let i = 2; i < n; i++) {
-    dp[i][0] = dp[i - 1][0] + pyramid[i][0];
-    dp[i][i] = dp[i - 1][i - 1] + pyramid[i][i];
-
-    for (let j = 1; j < i; j++) {
-      dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + pyramid[i][j];
-    }
-  }
-}
-
-console.log(Math.max(...dp[n - 1]));
+console.log(Math.max(...numbers[N - 1]));
