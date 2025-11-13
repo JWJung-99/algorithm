b85bdff7ad017ff14c4f150a7bcf6a0f96c6b56f
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-11-13 21:15:46 +0900

    [level 2] Title: 호텔 대실, Time: 23.96 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -1,3 +1,82 @@
+class PriorityQueue {
+	constructor() {
+		this.heap = [];
+	}
+
+	isEmpty() {
+		return this.heap.length === 0;
+	}
+
+	size() {
+		return this.heap.length;
+	}
+
+	peek() {
+		return this.heap[0] || null;
+	}
+
+	swap(idx1, idx2) {
+		[this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+	}
+
+	insert(time) {
+		this.heap.push(time);
+		this.bubbleUp();
+	}
+
+	bubbleUp() {
+		let index = this.heap.length - 1;
+		let parentIndex = Math.floor((index - 1) / 2);
+
+		while (
+			this.heap[parentIndex] &&
+			this.heap[index] < this.heap[parentIndex]
+		) {
+			this.swap(index, parentIndex);
+			index = parentIndex;
+			parentIndex = Math.floor((index - 1) / 2);
+		}
+	}
+
+	delete() {
+		if (this.isEmpty()) return null;
+
+		let rootNode = this.heap[0];
+		let lastNode = this.heap.pop();
+
+		if (!this.isEmpty()) {
+			this.heap[0] = lastNode;
+			this.bubbleDown();
+		}
+
+		return rootNode;
+	}
+
+	bubbleDown() {
+		let index = 0;
+		let leftIndex = index * 2 + 1;
+		let rightIndex = index * 2 + 2;
+
+		while (
+			(this.heap[leftIndex] && this.heap[leftIndex] < this.heap[index]) ||
+			(this.heap[rightIndex] && this.heap[rightIndex] < this.heap[index])
+		) {
+			let smallerIndex = leftIndex;
+			if (
+				this.heap[rightIndex] &&
+				this.heap[rightIndex] < this.heap[smallerIndex]
+			) {
+				smallerIndex = rightIndex;
+			}
+
+			this.swap(index, smallerIndex);
+			index = smallerIndex;
+			leftIndex = index * 2 + 1;
+			rightIndex = index * 2 + 2;
+		}
+	}
+}
+
 function solution(book_time) {
 	let N = book_time.length;
 	let arr = book_time
@@ -9,38 +88,14 @@ function solution(book_time) {
 		})
 		.sort((a, b) => a[0] - b[0]);
 
-	let visited = Array(N).fill(false);
+	let queue = new PriorityQueue();
 
-	function BFS(index) {
-		visited[index] = true;
-		let queue = [];
-		queue.push([arr[index], index]);
-
-		while (queue.length) {
-			let [prevTime, prevIndex] = queue.shift();
-
-			for (let i = prevIndex + 1; i < N; i++) {
-				if (visited[i]) continue;
-
-				let prevEnd = prevTime[1];
-				let newStart = arr[i][0];
-
-				if (newStart >= prevEnd + 10) {
-					visited[i] = true;
-					queue.push([arr[i], i]);
-                    break;
-				}
-			}
+	for (let [start, end] of arr) {
+		if (!queue.isEmpty() && queue.peek() + 10 <= start) {
+			queue.delete();
 		}
+		queue.insert(end);
 	}
 
-	let answer = 0;
-	for (let i = 0; i < N; i++) {
-		if (!visited[i]) {
-			answer++;
-			BFS(i);
-		}
-	}
-
-	return answer;
+	return queue.size();
 }
