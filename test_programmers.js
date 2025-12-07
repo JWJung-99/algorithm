77e64849f278e87b5847cefcaa63f179019e4f7d
JWJung-99 <39busy@naver.com>
2025-12-07 23:30:01 +0900

    Programmers: 프로그래머스 테스트 코드

--- test_programmers.js
+++ test_programmers.js
@@ -1,77 +1,8 @@
-function solution(picks, minerals) {
-	// minerals 배열을 5개씩 묶기
-	let groups = [];
-	for (let i = 0; i < minerals.length; i += 5) {
-		groups.push(minerals.slice(i, i + 5));
-	}
-
-	// 각 그룹의 광물을 캐는데 필요한 최대 피로도 계산(돌 곡괭이 기준) 후 정렬
-	let groupInfo = groups
-		.map((group) => {
-			let [dia, iron, stone] = [0, 0, 0];
-
-			group.forEach((element) => {
-				if (element === 'diamond') dia++;
-				else if (element === 'iron') iron++;
-				else stone++;
-			});
-
-			let score = dia * 25 + iron * 5 + stone * 1;
-
-			return [score, dia, iron, stone];
-		})
-		.sort((a, b) => b[0] - a[0]);
-
-	let answer = 0;
-	let index = 0;
-
-	for (let type = 0; type < 3; type++) {
-		while (picks[type] > 0 && index < groupInfo.length) {
-			let [score, dia, iron, stone] = groupInfo[index];
-
-			if (type === 0) answer += dia + iron + stone;
-			else if (type === 1) answer += dia * 5 + iron + stone;
-			else answer += score;
-
-			picks[type]--;
-			index++;
-		}
-	}
-
+function solution(diffs, times, limit) {
+	var answer = 0;
 	return answer;
 }
 
 // console.log(solution());
-console.log(
-	solution(
-		[1, 3, 2],
-		[
-			'diamond',
-			'diamond',
-			'diamond',
-			'iron',
-			'iron',
-			'diamond',
-			'iron',
-			'stone',
-		]
-	)
-);
-console.log(
-	solution(
-		[0, 1, 1],
-		[
-			'diamond',
-			'diamond',
-			'diamond',
-			'diamond',
-			'diamond',
-			'iron',
-			'iron',
-			'iron',
-			'iron',
-			'iron',
-			'diamond',
-		]
-	)
-);
+console.log(solution([1, 5, 3], [2, 4, 7], 30));
+console.log(solution([1, 328, 467, 209, 54], [2, 7, 1, 4, 3], 1723));
