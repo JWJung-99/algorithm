f378c254575d834767fd412916e971424d4a0cd2
정진욱 <84115816+JWJung-99@users.noreply.github.com>
2025-12-10 01:11:11 +0900

    [level 2] Title: N-Queen, Time: 489.17 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -0,0 +1,30 @@
+function solution(n) {
+	let answer = 0;
+	let queens = [];
+
+	function isPossible(x, y) {
+		for (let [a, b] of queens) {
+			if (x === a || y === b) return false;
+			if (Math.abs(x - a) === Math.abs(y - b)) return false;
+		}
+
+		return true;
+	}
+
+	function DFS(depth) {
+		if (depth === n) answer += 1;
+		else {
+			for (let i = 0; i < n; i++) {
+				if (!isPossible(depth, i)) continue;
+
+				queens.push([depth, i]);
+				DFS(depth + 1);
+				queens.pop();
+			}
+		}
+	}
+
+	DFS(0);
+
+	return answer;
+}
