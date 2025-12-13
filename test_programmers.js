1318c603aabc2083f8188dafe39a755c86b4a9cd
JWJung-99 <39busy@naver.com>
2025-12-13 19:52:23 +0900

    Programmers: 투포인터 알고리즘 복습

--- test_programmers.js
+++ test_programmers.js
@@ -1,33 +1,22 @@
-function solution(n) {
+function solution(num, m) {
 	let answer = 0;
-	let queens = [];
 
-	function isPossible(x, y) {
-		for (let [a, b] of queens) {
-			if (x === a || y === b) return false;
-			if (Math.abs(x - a) === Math.abs(y - b)) return false;
-		}
+	let start = 0;
+	let end = 0;
+	let sum = 0;
 
-		return true;
-	}
-
-	function DFS(depth) {
-		if (depth === n) answer += 1;
-		else {
-			for (let i = 0; i < n; i++) {
-				if (!isPossible(depth, i)) continue;
-
-				queens.push([depth, i]);
-				DFS(depth + 1);
-				queens.pop();
-			}
+	while (true) {
+		if (sum >= m) {
+			if (sum === m) answer++;
+			sum -= num[start++];
+		} else {
+			if (end === num.length) break;
+			sum += num[end++];
 		}
 	}
-
-	DFS(0);
 
 	return answer;
 }
 
 // console.log(solution());
-console.log(solution(4));
+console.log(solution([1, 2, 2, 3, 5], 5));
