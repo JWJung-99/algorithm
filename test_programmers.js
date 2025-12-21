864f42a29f7b4933e012ebf050628d656f91bd79
JWJung-99 <39busy@naver.com>
2025-12-21 16:24:05 +0900

    Programmers: 프로그래머스 테스트 코드

--- test_programmers.js
+++ test_programmers.js
@@ -1,22 +1,4 @@
-function solution(num, m) {
-	let answer = 0;
-
-	let start = 0;
-	let end = 0;
-	let sum = 0;
-
-	while (true) {
-		if (sum >= m) {
-			if (sum === m) answer++;
-			sum -= num[start++];
-		} else {
-			if (end === num.length) break;
-			sum += num[end++];
-		}
-	}
-
-	return answer;
-}
+function solution() {}
 
 // console.log(solution());
-console.log(solution([1, 2, 2, 3, 5], 5));
+console.log(solution());
